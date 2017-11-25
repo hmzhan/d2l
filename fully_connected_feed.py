f04064aba3104139b926b1ccc75d8f64335f9e47
Zhan <hzhan@massmutual.com>
2017-11-25 11:28:02 -0500

    update

--- fully_connected_feed.py
+++ fully_connected_feed.py
@@ -39,4 +39,41 @@ def fill_feed_dict(data_set, images_pl, labels_pl):
 	return feed_dict
 
 
+def do_eval(sess, 
+	        eval_correct, 
+	        images_placeholder, 
+	        labels_placeholder, 
+	        data_set):
+	true_count = 0
+	steps_per_epoch = data_set.num_examples // FLAGS.batch_size
+	num_examples = steps_per_epoch * FLAGS.batch_size
+	for step in xrange(steps_per_epoch):
+		feed_dict = fill_feed_dict(
+			data_set,
+			images_placeholder,
+			labels_placeholder
+		)
+		true_count += sess.run(eval_correct, feed_dict = feed_dict)
+	precision = float(true_count) / num_examples
+	print(' Num examples: %d Num correct: %d Precision @ 1: %0.04f' %
+		(num_examples, true_count, precision))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
