ced76e4848cd93229199eef0fe2d08fc6cd9f2f3
Zhan <hzhan@massmutual.com>
2017-11-25 14:29:49 -0500

    update

--- fully_connected_feed.py
+++ fully_connected_feed.py
@@ -59,17 +59,140 @@ def do_eval(sess,
 		(num_examples, true_count, precision))
 
 
+def run_training():
+	datasets = input_data.read_data_sets(
+		FLAGS.input_data_dir,
+		FLAGS.fake_data
+	)
+	with tf.Graph().as_default():
+		images_placeholder, labels_placeholder = placeholder_inputs(
+			FLAGS.batch_size
+		)
+		logits = mnist.inference(
+			images_placeholder,
+			FLAGS.hidden1,
+			FLAGS.hidden2
+		)
+		loss = mnist.loss(logits, labels_placeholder)
+		train_op = mnist.training(loss, FLAGS.learning_rate)
+		eval_correct = mnist.evaluation(logits, labels_placeholder)
+		summary = tf.summary.merge_all()
+		init = tf.global_variables_initializer()
+		saver = tf.train.Saver()
+		sess = tf.Session()
+		summary_writer = tf.summary.FileWriter(
+			FLAGS.log_dir,
+			sess.graph 
+		)
+		sess.run(init)
+		for step in xrange(FLAGS.max_steps):
+			start_time = time.time()
+			feed_dict = fill_feed_dict(
+				data_sets.train,
+				images_placeholder,
+				labels_placeholder
+			)
+			_, loss_value = sess.run(
+				[train_op, loss],
+				feed_dict = feed_dict
+			)
+			duration = time.time() - start_time
+			if step % 100 == 0:
+				print('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration))
+				summary_str = sess.run(summary, feed_dict = feed_dict)
+				summary_writer.add_summary(summary_str, step)
+				summary_writer.flush()
+			if (step + 1) % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+				checkpoint_file = os.path.join(FLAGS.log_dir, 'model.ckpt')
+				saver.save(sess, checkpoint_file, global_step = step)
+				print('Training Data Eval:')
+				do_eval(
+					sess,
+					eval_correct,
+					images_placeholder,
+					labels_placeholder,
+					data_sets.train
+				)
+				print('Validation Data Eval:')
+				do_eval(
+					sess,
+					eval_correct,
+					images_placeholder,
+					labels_placeholder,
+					data_sets.validation
+				)
+				print('Test Data Eval:')
+				do_eval(
+					sess,
+					eval_correct,
+					images_placeholder,
+					labels_placeholder,
+					data_sets.test
+				)
 
 
+def main(_):
+	if tf.gfile.Exists(FLAGS.log_dir):
+		tf.gfile.DeleteRecursively(FLAGS.log_dir)
+	tf.gfile.MakeDirs(FLAGS.log_dir)
+	run_training()
 
 
+if __name__ == '__main__':
+	  parser = argparse.ArgumentParser()
+      parser.add_argument(
+          '--learning_rate',
+          type=float,
+          default=0.01,
+          help='Initial learning rate.'
+      )
+      parser.add_argument(
+          '--max_steps',
+          type=int,
+          default=2000,
+          help='Number of steps to run trainer.'
+      )
+      parser.add_argument(
+          '--hidden1',
+          type=int,
+          default=128,
+          help='Number of units in hidden layer 1.'
+      )
+      parser.add_argument(
+          '--hidden2',
+          type=int,
+          default=32,
+          help='Number of units in hidden layer 2.'
+      )
+      parser.add_argument(
+          '--batch_size',
+          type=int,
+          default=100,
+          help='Batch size.  Must divide evenly into the dataset sizes.'
+      )
+      parser.add_argument(
+          '--input_data_dir',
+          type=str,
+          default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'),
+                               'tensorflow/mnist/input_data'),
+          help='Directory to put the input data.'
+      )
+      parser.add_argument(
+          '--log_dir',
+          type=str,
+          default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'),
+                               'tensorflow/mnist/logs/fully_connected_feed'),
+          help='Directory to put the log data.'
+      )
+      parser.add_argument(
+          '--fake_data',
+          default=False,
+          help='If true, uses fake data for unit testing.',
+          action='store_true'
+      )
 
-
-
-
-
-
-
+      FLAGS, unparsed = parser.parse_known_args()
+      tf.app.run(main =main, argv = [sys.argv[0] + unparsed])
 
 
 
