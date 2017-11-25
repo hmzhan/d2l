cb28f13b2f972b2de808b6a1abcf6fffd401696e
Zhan <hzhan@massmutual.com>
2017-11-25 14:31:27 -0500

    update

--- fully_connected_feed.py
+++ fully_connected_feed.py
@@ -139,60 +139,58 @@ def main(_):
 
 
 if __name__ == '__main__':
-	  parser = argparse.ArgumentParser()
-      parser.add_argument(
-          '--learning_rate',
-          type=float,
-          default=0.01,
-          help='Initial learning rate.'
-      )
-      parser.add_argument(
-          '--max_steps',
-          type=int,
-          default=2000,
-          help='Number of steps to run trainer.'
-      )
-      parser.add_argument(
-          '--hidden1',
-          type=int,
-          default=128,
-          help='Number of units in hidden layer 1.'
-      )
-      parser.add_argument(
-          '--hidden2',
-          type=int,
-          default=32,
-          help='Number of units in hidden layer 2.'
-      )
-      parser.add_argument(
-          '--batch_size',
-          type=int,
-          default=100,
-          help='Batch size.  Must divide evenly into the dataset sizes.'
-      )
-      parser.add_argument(
-          '--input_data_dir',
-          type=str,
-          default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'),
-                               'tensorflow/mnist/input_data'),
-          help='Directory to put the input data.'
-      )
-      parser.add_argument(
-          '--log_dir',
-          type=str,
-          default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'),
-                               'tensorflow/mnist/logs/fully_connected_feed'),
-          help='Directory to put the log data.'
-      )
-      parser.add_argument(
-          '--fake_data',
-          default=False,
-          help='If true, uses fake data for unit testing.',
-          action='store_true'
-      )
-
-      FLAGS, unparsed = parser.parse_known_args()
-      tf.app.run(main =main, argv = [sys.argv[0] + unparsed])
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+	    '--learning_rate',
+	    type=float,
+	    default=0.01,
+	    help='Initial learning rate.'
+	)
+	parser.add_argument(
+	    '--max_steps',
+	    type=int,
+	    default=2000,
+	    help='Number of steps to run trainer.'
+	)
+	parser.add_argument(
+	    '--hidden1',
+	    type=int,
+	    default=128,
+	    help='Number of units in hidden layer 1.'
+	)
+	parser.add_argument(
+	    '--hidden2',
+	    type=int,
+	    default=32,
+	    help='Number of units in hidden layer 2.'
+	)
+	parser.add_argument(
+	    '--batch_size',
+	    type=int,
+	    default=100,
+	    help='Batch size.  Must divide evenly into the dataset sizes.'
+	)
+	parser.add_argument(
+	    '--input_data_dir',
+	    type=str,
+	    default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'),
+	                         'tensorflow/mnist/input_data'),
+	    help='Directory to put the input data.'
+	)
+	parser.add_argument(
+	    '--log_dir',
+	    type=str,
+	    default=os.path.join(os.getenv('TEST_TMPDIR', '/tmp'),
+	                         'tensorflow/mnist/logs/fully_connected_feed'),
+	    help='Directory to put the log data.'
+	)
+	parser.add_argument(
+	    '--fake_data',
+	    default=False,
+	    help='If true, uses fake data for unit testing.',
+	    action='store_true'
+	)	FLAGS, unparsed = parser.parse_known_args()
+	tf.app.run(main =main, argv = [sys.argv[0] + unparsed])
 
 
 
