4ae13986563493a3c0f5212d254dd46a57f41b6a
hmzhan <hmzhan@gmail.com>
2024-02-05 07:53:52 -0500

    add RNNLMScratch

--- d2l_rnn.py
+++ d2l_rnn.py
@@ -2,7 +2,10 @@ import math
 import torch
 from torch import nn
 from torch.nn import functional as F
-from .utils import Module
+from .utils import (
+    Module,
+    Classifier
+)
 
 
 class RNNScratch(Module):
@@ -23,3 +26,23 @@ class RNNScratch(Module):
             state = torch.tanh(torch.matmul(X, self.W_xh) + torch.matmul(state, self.W_hh) + self.b_h)
             outputs.append(state)
         return outputs, state
+
+
+class RNNLMScratch(Classifier):
+    def __init__(self, rnn, vocab_size, lr=0.01):
+        super().__init__()
+        self.save_hyperparameters()
+        self.init_params()
+
+    def init_params(self):
+        self.W_hq = nn.Parameter(torch.randn(self.rnn.num_hiddens, self.vocab_size) * self.rnn.sigma)
+        self.b_q = nn.Parameter(torch.zeros(self.vocab_size))
+
+    def training_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('ppl', torch.exp(1), train=True)
+        return l
+
+    def validation_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('ppl', torch.exp(1), train=False)
--- utils.py
+++ utils.py
@@ -239,3 +239,17 @@ def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
         if titles:
             ax.set_title(titles[i])
     return axes
+
+
+class Classifier(Module):
+    def validation(self, batch):
+        Y_hat = self(*batch[:-1])
+        # self.plot('loss', self.loss(Y_hat, batch[-1]), train=False)
+        # self.plot('acc', self.accuracy(Y_hat, batch[-1]), train=False)
+
+    def accuracy(self, Y_hat, Y, averaged=True):
+        """Compute the number of correct predictions."""
+        Y_hat = Y_hat.reshape((-1, Y_hat.shape[-1]))
+        preds = Y_hat.argmax(axis=1).type(Y.dtype)
+        compare = (preds == Y.reshape(-1)).type(torch.float32)
+        return compare.mean() if averaged else compare
