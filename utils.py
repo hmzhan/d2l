eecf9f405d66c664b48a58584b0e219687347318
hmzhan <hmzhan@gmail.com>
2024-06-27 22:50:19 -0400

    add MNIST to utils.py

--- utils.py
+++ utils.py
@@ -420,3 +420,33 @@ class FashionMNIST(DataModule):
 
 
 reshape = lambda x, *args, **kwargs: x.reshape(*args, **kwargs)
+
+
+class MNIST(DataModule):
+    def __init__(self, batch_size=64, resize=(28, 28)):
+        super().__init__()
+        self.save_hyperparameters()
+        trans = transforms.Compose([transforms.Resize(resize),
+                                    transforms.ToTensor()])
+        self.train = torchvision.datasets.MNIST(
+            root=self.root, train=True, transform=trans, download=True)
+        self.val = torchvision.datasets.MNIST(
+            root=self.root, train=False, transform=trans, download=True)
+
+    def text_labels(self, indices):
+        """Return text labels."""
+        labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        return [labels[int(i)] for i in indices]
+
+    def get_dataloader(self, train):
+        data = self.train if train else self.val
+        return torch.utils.data.DataLoader(data, self.batch_size, shuffle=train,
+                                           num_workers=self.num_workers)
+
+    def visualize(self, batch, nrows=1, ncols=8, labels=[]):
+        X, y = batch
+        if not labels:
+            labels = self.text_labels(y)
+        show_images(X.squeeze(1), nrows, ncols, titles=labels)
+
+
